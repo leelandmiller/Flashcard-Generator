78dd81734fc8f58f66eb90946e55321e6718deb3
Leeland <leelandmiller@gmail.com>
2017-06-28 22:01:37 -0700

    added error checking for ClozeCard

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,8 +1,20 @@
 function ClozeCard(text, cloze) {
-    this.cloze = cloze;
-    this.fullText = text;
-    this.partialText = this.setPartialText(text, cloze);
+    if (this.isValid(text, cloze)) {
+        this.partialText = this.setPartialText(text, cloze);
+        this.cloze = cloze;
+        this.fullText = text;
+    } else {
+        console.log('Error. The text does not contain the cloze text that was given');
+    }
 }
+
+ClozeCard.prototype.isValid = function (text, cloze) {
+    if (text.includes(cloze)) {
+        return true;
+    } else {
+        return false;
+    }
+};
 
 ClozeCard.prototype.setPartialText = function (text, cloze) {
     return text.replace(cloze, '...');
